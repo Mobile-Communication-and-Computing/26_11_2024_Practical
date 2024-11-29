34eb339979a528bd1825f96100c76d3275559dc4
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2024-11-29 14:03:32 +0000

    Created routes to components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<AboutUs />} />
+          <Route path='/contact' element={<Contactus />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
